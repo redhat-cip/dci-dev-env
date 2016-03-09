b928739213075d330873a5e049184e05e93a885b
Cedric LECOMTE <clecomte@redhat.com>
2016-03-09 11:07:40 -0500

    Add a script to manage all the boostrap in one exec
    Change-Id: I3774d740520a05eab05d07cb96a70ab2a1966fbf

---
+++ utils/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+if [ ! -f dci.yml ]; then
+    echo "You seems to be in the wrong directory"
+    echo "Execute this script from the root of dci-dev-env with ./utils/boostrap.sh"
+    exit 1
+fi
+
+git clone http://softwarefactory-project.io/r/dci-control-server
+git clone http://softwarefactory-project.io/r/python-dciclient
+git clone http://softwarefactory-project.io/r/dci-ui
+git clone http://softwarefactory-project.io/r/dci-doc
