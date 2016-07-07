99f9f471012329f60193d918fb315d2152de4210
François Charlier <francois.charlier@redhat.com>
2016-07-07 14:42:54 +0200

    Doc: fix building DOC container
    rcip-doc commit b633a28528097c9f53d0ed602611990cca2c1a4b added the
    python-dciclient as a dependency which prevented the container from
    being built with the current configuration because the volume specified
    in the docker-compose configuration is not available at build time.
    
    - Install new package dependencies
    - Use /tmp as a workdir while building the container
    - Add local python-dciclient dependency for pip
    
    Change-Id: I7a18948846fbc43fbdcf6161d8095a3be1e6048f

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -9,4 +9,4 @@ fi
 git clone http://softwarefactory-project.io/r/dci-control-server
 git clone http://softwarefactory-project.io/r/python-dciclient
 git clone http://softwarefactory-project.io/r/dci-ui
-git clone http://softwarefactory-project.io/r/dci-doc
+git clone --recurse-submodules http://softwarefactory-project.io/r/dci-doc
