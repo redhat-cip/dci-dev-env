3317d676ccb342acfdba906f7c87cd99e3471b97
George Peristerakis <gperiste@redhat.com>
2016-07-12 14:52:18 -0400

    Fix the docker DB settings
    Change-Id: Ie24705178037199ec630fe2c276d7738eb075e6f

--- settings/settings.py
+++ settings/settings.py
@@ -22,7 +22,7 @@ HOST = '0.0.0.0'
 db_url = six.moves.urllib_parse.urlparse(os.environ.get('DB_PORT'))
 
 SQLALCHEMY_DATABASE_URI = (
-    'postgresql://dci:password@%s/dci_control_server' % db_url.netloc
+    'postgresql://dci:dci@%s/dci' % db_url.netloc
 )
 
 SQLALCHEMY_ECHO = False
