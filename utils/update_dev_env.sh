727b82c3bb6e5e1a07189ab6b4fcd2413b94e2fa
Guillaume Vincent <gvincent@redhat.com>
2017-03-14 11:20:56 +0100

    add update_dev_env.sh util script
    Change-Id: I54e588b59eb3e926d0db88b90b2e9cbc341c3466

---
+++ utils/update_dev_env.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -eux
+
+if [ ! -f dci.yml ]; then
+    echo "You seems to be in the wrong directory"
+    echo "Execute this script from the root of dci-dev-env with ./utils/update_dev_env.sh"
+    exit 1
+fi
+
+function clean_repository {
+    git pull origin master
+    git checkout master
+    git reset --hard origin/master
+}
+
+read -p "This script will reset hard all your repositories, rebuild containers, clean docker volumes. Are you sure you want to continue? (y/N)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+
+    submodules="dci-control-server dci-doc dci-ui python-dciclient"
+    for submodule in ${submodules}
+    do
+        [ -d $submodule ] || git clone http://github.com/redhat-cip/${submodule}
+        cd $submodule
+        clean_repository
+        cd ..
+    done
+
+    dc="docker-compose -f dci.yml -f dci-db_init.yml -f dci-swift.yml"
+    ${dc} pull
+    ${dc} build
+    ${dc} down -v
+    ${dc} up -d
+
+fi
