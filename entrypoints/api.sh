e15776404a421b29362ec19a7121eb9495a5d278
Guillaume Vincent <gvincent@redhat.com>
2017-09-13 16:51:50 +0200

    Don't provision database when container starts
    Change-Id: I04f088b12d17236628b4db6f369735e9955d54a6

--- entrypoints/api.sh
+++ entrypoints/api.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python /opt/dci-control-server/scripts/db_provisioning.py -y
+DCI_LOGIN='admin' DCI_PASSWORD='admin' python /opt/dci-control-server/bin/dci-dbinit
 
 python /opt/keycloak-provision.py
 
