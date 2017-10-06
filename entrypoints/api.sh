4e504bfa94d0ede2a85c4044b6fcdfc956d19151
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2017-10-06 13:56:57 +0200

    api: register the keycloak public key
    Adds the SSO_PUBLIC_KEY configuration value
    in the settings of the api container.
    
    Change-Id: I332671a418982b7eaefb16ef894dcf100c79cf14

--- entrypoints/api.sh
+++ entrypoints/api.sh
@@ -4,4 +4,10 @@ DCI_LOGIN='admin' DCI_PASSWORD='admin' python /opt/dci-control-server/bin/dci-db
 
 python /opt/keycloak-provision.py
 
+pubkey=$(python bin/dci-get-pem-ks-key.py http://keycloak:8180 dci-test)
+
+echo -n 'SSO_PUBLIC_KEY = """' >> /tmp/settings/settings.py
+echo "$pubkey" >> /tmp/settings/settings.py
+echo '"""' >> /tmp/settings/settings.py
+
 exec "$@"
