b4f37fe8b73dcb9ce4a7c5de285a96acb1bfd44c
Guillaume Vincent <gvincent@redhat.com>
2018-03-13 10:37:36 +0100

    Create a git status script to see projects status
    Change-Id: I8048d5222150d183bfa57894b3e3c7c1cdaa36d8

---
+++ utils/git-status.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+NOCOLOR='\033[0m'
+
+if [ ! -f dci.yml ]; then
+    echo -e "${RED}You seems to be in the wrong directory"
+    echo -e "Execute this script from the root of dci-dev-env with ./utils/${0##*/}${NOCOLOR}"
+    exit 1
+fi
+
+function check_uncommitted_changes {
+    pushd ${1:-"."} &> /dev/null
+    local=$(git rev-parse master)
+    remote=$(git rev-parse origin/master)
+    base=$(git merge-base master origin/master)
+    project_name=${1:-"dci-dev-env"}
+    if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
+        printf "║ %-18s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "uncommitted changes"
+    elif [ ${local} = ${remote} ]; then
+        printf "║ %-18s │ ${GREEN}%-20s${NOCOLOR} ║\n" "${project_name}" "up to date"
+    elif [ ${local} = ${base} ]; then
+        printf "║ %-18s │ ${ORANGE}%-20s${NOCOLOR} ║\n" "${project_name}" "need to pull"
+    elif [ ${remote} = ${base} ]; then
+        printf "║ %-18s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "need to push"
+    else
+        printf "║ %-18s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "diverged"
+    fi
+    popd &> /dev/null
+}
+printf "╔════════════════════╤══════════════════════╗\n"
+check_uncommitted_changes
+printf "╟────────────────────┼──────────────────────╢\n"
+PROJECTS="dci-control-server dci-ui dci-doc python-dciclient"
+for PROJECT in ${PROJECTS}
+do
+    check_uncommitted_changes ${PROJECT}
+    printf "╟────────────────────┼──────────────────────╢\n"
+done
+check_uncommitted_changes "python-dciauth"
+printf "╚════════════════════╧══════════════════════╝\n"
