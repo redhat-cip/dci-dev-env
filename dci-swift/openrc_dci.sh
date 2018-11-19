d2fb01ccce4f1567e4ac46fa226526e449c43df8
Gonéri Le Bouder <goneri@redhat.com>
2018-11-19 17:53:23 -0500

    swift: fix the config permission + doc
    Give access to httpd to Keystone and Swift configuration files.
    
    This commit also explain how to interact with the Swift instance.
    
    Change-Id: I215001b0f2f141f094db3d614ea59e94c46db0c4

---
+++ dci-swift/openrc_dci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export OS_AUTH_URL=http://127.0.0.1:5001/v2.0
+export OS_TENANT_NAME="dci"
+export OS_PROJECT_NAME="dci"
+export OS_USERNAME="dci"
+export OS_PASSWORD="dci"
+export OS_REGION_NAME="regionOne"
