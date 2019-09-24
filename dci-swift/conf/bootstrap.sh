e1d6421bb4aa0919bd94d4a3ac764b326408e468
François Charlier <francois.charlier@redhat.com>
2019-09-24 15:42:22 +0200

    Update Swift & Keystone config for RDO Rocky
    RDO Pike is now archived, need to move to RDO Rocky for Keystone &
    Swift.
    
    There were some changes with Keystone in Rocky:
    - Keystone: No need to listen on different port for admin API, removing
    35357 port
    - Keystone: Move to API v3 only, different way to initialize users, etc…
    - Swift/Keystoneauth: Change parameters to use non-deprecated ones.
    - Update openrc_dci with new parameters
    
    Also change Keystone listen port to 5001 everywhere to prevent
    headaches.
    
    Change-Id: I17f1da472b5105e3bb7b0cf1f6519520f958dde7

--- dci-swift/conf/bootstrap.sh
+++ dci-swift/conf/bootstrap.sh
@@ -12,6 +12,7 @@ touch /var/log/keystone/keystone.log
 chown keystone:keystone /var/log/keystone/keystone.log
 keystone-manage db_sync
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
 
 systemctl enable httpd
 systemctl start httpd
@@ -31,20 +32,45 @@ swift-ring-builder /etc/swift/account.builder rebalance
 chown -R swift:swift /srv/node
 
 # Create all User / Service / Endpoints
-AUTH="--os-url=http://localhost:35357/v2.0/ --os-token=root"
 IP=`ifconfig | grep -A1 eth0 | grep inet | awk '{print $2}'`
-openstack role create admin $AUTH
-openstack role create SwiftOperator $AUTH
-openstack project create service $AUTH
-openstack project create dci $AUTH
-openstack user create admin --project service $AUTH
-openstack user create swift --project service --password swift $AUTH
-openstack user create dci --project dci --password dci $AUTH
-openstack role add --project service --user swift admin $AUTH
-openstack role add --project service --user admin admin $AUTH
-openstack role add --project dci --user dci SwiftOperator $AUTH
-openstack service create --name swift --description "OpenStack Object Storage" object-store $AUTH
-openstack endpoint create --region regionOne object-store --publicurl http://$IP:8080/v1/AUTH_%\(tenant_id\)s --internalurl http://$IP:8080/v1/AUTH_%\(tenant_id\)s --adminurl http://$IP:8080/v1 $AUTH
+
+keystone-manage bootstrap \
+    --bootstrap-password admin \
+    --bootstrap-admin-url http://${IP}:5001/v3/ \
+    --bootstrap-internal-url http://${IP}:5001/v3/ \
+    --bootstrap-public-url http://${IP}:5001/v3/ \
+    --bootstrap-region-id RegionOne
+
+export OS_USERNAME=admin
+export OS_PASSWORD=admin
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_AUTH_URL=http://${IP}:5001/v3
+export OS_IDENTITY_API_VERSION=3
+
+openstack project create --domain default --description "Service Project" service
+openstack project create --domain default --description "DCI Project" dci
+
+# openstack role create user
+openstack role create service
+openstack role create member
+openstack role create user
+openstack role create swiftoperator
+
+openstack user create --project service --password swift swift
+openstack role add --project service --user swift service
+openstack role add --project service --user admin admin
+
+openstack user create --project dci --password dci dci
+openstack role add --project dci --user dci member
+openstack role add --project dci --user dci user
+openstack role add --project dci --user dci swiftoperator
+
+openstack service create --name swift --description "OpenStack Object Storage" object-store
+openstack endpoint create --region regionOne object-store public http://$IP:8080/v1/AUTH_%\(tenant_id\)s
+openstack endpoint create --region regionOne object-store internal http://$IP:8080/v1/AUTH_%\(tenant_id\)s
+openstack endpoint create --region regionOne object-store admin http://$IP:8080/v1
 
 # Remove ipv6 binding
 sed -i -e "s/,::1//" /etc/sysconfig/memcached
--- dci-swift/openrc_dci.sh
+++ dci-swift/openrc_dci.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export OS_AUTH_URL=http://0.0.0.0:5001/v2.0
-export OS_TENANT_NAME="dci"
-export OS_PROJECT_NAME="dci"
-export OS_USERNAME="dci"
-export OS_PASSWORD="dci"
-export OS_REGION_NAME="regionOne"
+export OS_USERNAME=dci
+export OS_PASSWORD=dci
+export OS_PROJECT_NAME=dci
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_AUTH_URL=http://0.0.0.0:5001/v3
+export OS_IDENTITY_API_VERSION=3
