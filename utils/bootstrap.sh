997ab8e47ad64fa7a333b17d41d02c8961726c5c
Guillaume Vincent <gvincent@redhat.com>
2019-10-22 09:45:38 +0200

    Use software factory instead of github in the bootstrap script
    Change-Id: Idc4c434ff65c560aa7025bb968313cb531441371

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -9,13 +9,19 @@ if [ ! -f dci.yml ]; then
     exit 1
 fi
 
-[ -d dci-control-server ] || git clone https://softwarefactory-project.io/r/dci-control-server
-[ -d python-dciclient ] || git clone https://softwarefactory-project.io/r/python-dciclient
-[ -d python-dciauth ] || git clone https://softwarefactory-project.io/r/python-dciauth
-[ -d dci-ui ] || git clone https://softwarefactory-project.io/r/dci-ui
-[ -d dci-doc ] || git clone --recurse-submodules https://softwarefactory-project.io/r/dci-doc
-[ -d dci-ansible ] || git clone https://softwarefactory-project.io/r/dci-ansible
-[ -d dci-downloader ] || git clone https://softwarefactory-project.io/r/dci-downloader
-[ -d dci-openstack-agent ] || git clone https://softwarefactory-project.io/r/dci-openstack-agent
-[ -d dci-rhel-agent ] || git clone https://softwarefactory-project.io/r/dci-rhel-agent
-[ -d ansible-playbook-dci-beaker ] || git clone https://softwarefactory-project.io/r/ansible-playbook-dci-beaker
+echo What is your Software Factory username?
+
+read softwarefactory_username
+
+git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-dev-env.git
+cd dci-dev-env
+[ -d dci-control-server ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-control-server
+[ -d python-dciclient ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/python-dciclient
+[ -d python-dciauth ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/python-dciauth
+[ -d dci-ui ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-ui
+[ -d dci-doc ] || git clone --recurse-submodules ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-doc
+[ -d dci-ansible ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-ansible
+[ -d dci-downloader ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-downloader
+[ -d dci-openstack-agent ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-openstack-agent
+[ -d dci-rhel-agent ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/dci-rhel-agent
+[ -d ansible-playbook-dci-beaker ] || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/ansible-playbook-dci-beaker
