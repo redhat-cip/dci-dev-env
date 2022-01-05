4510213c67c22f1a1b7eb4ec2ce2172f6c3b904b
Haikel Guemar <hguemar@fedoraproject.org>
2022-01-05 13:08:54 +0100

    Fix warning in execution of bootstrap.sh
    Warning: "utils/bootstrap.sh: line 18: [: -l: unary operator expected"
    Effectively, -l is not a valid unary operator in Bash, but -L tests if
    a symbolic link exists.
    
    Change-Id: I7a1ec7f2db0ee3fffb2b70991efe913f98fb3798

--- utils/bootstrap.sh
+++ utils/bootstrap.sh
@@ -15,5 +15,5 @@ read softwarefactory_username
 
 cat utils/projects.lst | while read project
 do
-    ([ -d "$project" ] || [ -l "$project" ]) || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/$project
+    ([ -d "$project" ] || [ -L "$project" ]) || git clone ssh://${softwarefactory_username}@softwarefactory-project.io:29418/$project
 done
