0b161a027997634e3e4941f71528c3242f5aca46
François Charlier <francois.charlier@redhat.com>
2023-04-13 12:06:49 +0200

    Add a status for non existent directories
    Change-Id: I5da48e0d09c9c283beb0ddbca47c395bc9d6beb1

--- utils/git-status.sh
+++ utils/git-status.sh
@@ -3,6 +3,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
+PURPLE='\033[0;35m'
 NOCOLOR='\033[0m'
 
 if [ ! -f dci.yml ]; then
@@ -12,21 +13,25 @@ if [ ! -f dci.yml ]; then
 fi
 
 function check_uncommitted_changes {
+    project_name=${1:-"dci-dev-env"}
+    if [ ! -d ${1:-"."} ]; then
+        printf "║ %-25s │ ${PURPLE}%-20s${NOCOLOR} ║\n" "${project_name}" "does not exist"
+        return
+    fi
     pushd ${1:-"."} &> /dev/null
     local=$(git rev-parse master)
     remote=$(git rev-parse origin/master)
     base=$(git merge-base master origin/master)
-    project_name=${1:-"dci-dev-env"}
     if [[ -n $(git status --porcelain 2>/dev/null) ]]; then
-        printf "║ %-20s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "uncommitted changes"
+        printf "║ %-25s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "uncommitted changes"
     elif [ ${local} = ${remote} ]; then
-        printf "║ %-20s │ ${GREEN}%-20s${NOCOLOR} ║\n" "${project_name}" "up to date"
+        printf "║ %-25s │ ${GREEN}%-20s${NOCOLOR} ║\n" "${project_name}" "up to date"
     elif [ ${local} = ${base} ]; then
-        printf "║ %-20s │ ${ORANGE}%-20s${NOCOLOR} ║\n" "${project_name}" "need to pull"
+        printf "║ %-25s │ ${ORANGE}%-20s${NOCOLOR} ║\n" "${project_name}" "need to pull"
     elif [ ${remote} = ${base} ]; then
-        printf "║ %-20s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "need to push"
+        printf "║ %-25s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "need to push"
     else
-        printf "║ %-20s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "diverged"
+        printf "║ %-25s │ ${RED}%-20s${NOCOLOR} ║\n" "${project_name}" "diverged"
     fi
     popd &> /dev/null
 }
@@ -46,11 +51,11 @@ do
 done
 wait
 
-printf "╔══════════════════════╤══════════════════════╗\n"
+printf "╔═══════════════════════════╤══════════════════════╗\n"
 check_uncommitted_changes
 cat utils/projects.lst | while read project
 do
-    printf "╟──────────────────────┼──────────────────────╢\n"
+    printf "╟───────────────────────────┼──────────────────────╢\n"
     check_uncommitted_changes ${project}
 done
-printf "╚══════════════════════╧══════════════════════╝\n"
+printf "╚═══════════════════════════╧══════════════════════╝\n"
